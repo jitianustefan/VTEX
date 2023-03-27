98e321d9d6e666943e00d5a413a1872e7fcb9812
jitianustefan <jitianu_stefan@yahoo.com>
2023-03-27 16:49:07 +0300

    commit custom block

--- store-block-template/react/Countdown.tsx
+++ store-block-template/react/Countdown.tsx
@@ -2,18 +2,22 @@ import React, {useState} from 'react'
 import { TimeSplit } from './typings/global'
 import { tick, getTwoDaysFromNow } from './utils/time'
 import { useCssHandles } from 'vtex.css-handles'
- 
+//import { FormattedMessage } from 'react-intl'
+import {useQuery} from 'react-apollo'
+import useProduct from 'vtex.product-context/useProduct'
+import productReleaseDate from './graphql/productReleaseDate.graphql'
+
+
 const DEFAULT_TARGET_DATE = getTwoDaysFromNow()
 const CSS_HANDLES = ['countdown']
 
 
+// interface CountdownProps {
+//   targetDate: string
+// }
 
-
-interface CountdownProps {
-  targetDate: string
-}
-
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ targetDate = DEFAULT_TARGET_DATE}) => {
+const Countdown: StorefrontFunctionComponent = () => {            //<CountdownProps>        
+                                                    //targetDate = DEFAULT_TARGET_DATE
   const [timeRemaining, setTime] = useState<TimeSplit>({
     hours: '00',
     minutes: '00',
@@ -22,14 +26,48 @@ const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ targetDate = D
 
   const handles = useCssHandles(CSS_HANDLES)
 
-  tick(targetDate, setTime)
+  const {product} = useProduct()
+  const {data, loading, error} = useQuery(productReleaseDate, {
+    variables: {
+      slug: product?.linkText
+    },
+    ssr:false
+  })
 
+  if(!product){
+    return (
+      <div>
+        <span>There is no product context</span>
+      </div>
+    )
+  }
+  if(loading) {
+    return (
+      <div>
+        <span>Loading...</span>
+      </div>
+    )
+  }
+  if(error) {
+    return (
+      <div>
+        <span>Error!</span>
+      </div>
+    )
+  }
+  console.log({data})
+  //const titleText = title || <FormattedMessage id="countdown.title"/>
+  //tick(targetDate, setTime)
 
+  tick(data?.product?.relseaseDate || DEFAULT_TARGET_DATE, setTime)
   
   return (
-<div className={`${handles.countdown} c-muted-1 db tc`}>
-    <h1>{` ${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}` }</h1>
-  </div>
+  // <div className={`${handles.container} t-heading-2 fw3 w-100 c-muted-1`}> 
+  //   <div className={`${handles.title} db tc`}>{titleText}</div>
+    <div className={`${handles.countdown} c-muted-1 db tc`}>
+      <h1>{` ${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}` }</h1>
+    </div>
+  //</div> 
 )
 }
 
@@ -43,6 +81,11 @@ Countdown.schema = {
       description: 'Final date used in the countdown',
       type: 'string',
       default: null,
+    },
+    title: {
+      title: 'I am a title JS',
+      type: 'string',
+      default: null
     },
   },
 }
