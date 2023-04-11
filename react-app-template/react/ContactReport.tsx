562734442e5e6161e89b4a275a89fa9e66344a0e
jitianustefan <jitianu_stefan@yahoo.com>
2023-04-11 18:37:16 +0300

    count module commit

---
+++ react-app-template/react/ContactReport.tsx
@@ -0,0 +1,23 @@
+//react/adminExample.tsx
+import React, { FC } from 'react'
+
+const ContactReport: FC = () => {
+  return (
+    <table>
+  <tr>
+    <th>Firstname</th>
+    <th>Lastname</th>
+  </tr>
+  <tr>
+    <td>Peter</td>
+    <td>Griffin</td>
+  </tr>
+  <tr>
+    <td>Lois</td>
+    <td>Griffin</td>
+  </tr>
+</table>
+  )
+}
+
+export default ContactReport
